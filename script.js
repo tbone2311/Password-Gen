355e3e530149bf99ca75b80b599f927dac5f613e
tbone2311 <garlinskit@yahoo.com>
2023-05-04 19:50:51 -0400

    Defined generatePassword function and user prompt for password length and validate input

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // Assignment code here
+
+// Vars for special character for function
 const lowercases = "abcdefghijklmnopqrstuvwxyz";
 const uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const specialCharacters = "!@#$%^&*()";
 const generateButton = document.getElementById("#generateBtn");
+
+// function for prompt after click "Generate Password"
+function generatePassword() {
+
+    var passwordLength = prompt("Please enter the number of characters you want to have no less than 8 no more than 128");
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("Invalid, Must be between 8 - 128 characters!");
+        return;
+    }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
